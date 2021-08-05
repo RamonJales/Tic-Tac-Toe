4c9a132cfa496ef1f9f0c7764a82d6f83f09ff8f
RamonJales <ramonjales123@gmail.com>
2021-08-05 17:16:04 -0300

    Defensive programming and BoardException class

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -7,6 +7,10 @@ public class Board {
 	private Figure[][] figures;
 	
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+		}
+		
 		this.rows = rows;
 		this.columns = columns;
 		figures = new Figure[rows][columns];
@@ -21,17 +25,43 @@ public class Board {
 	}
 	
 	public Figure figure(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Position not on the board");
+		}
+		
 		return figures[row][column];
 	}
 	
 	public Figure figure(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		
 		return figures[position.getRow()][position.getColumn()];
 	}
 	
 	public void placeFigure(Figure figure, Position position) {
+		if (thereIsAFigure(position)) {
+			throw new BoardException("you cannot place a figure in position: "+ position);
+		}
+		
 		figures[position.getRow()][position.getColumn()] = figure;
 		figure.position = position;
 	}
 	
+	public boolean positionExists(int row, int column) {
+		return row > 0 && row < rows && column > 0 && column < columns;
+	}
 	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAFigure(Position position) {
+		
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		return figure(position) != null;
+	}
 }
